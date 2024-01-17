4a960d80ba53dd4660bb4581413a1a3cbf79188a
onlyonepen <kritramet1234@gmail.com>
2024-01-17 19:33:26 +0700

    HOLY FUCK I FINALLY CAN MOVE THE FUCKING OBJECT

--- Assets/ObjectMoverManagerScript.cs
+++ Assets/ObjectMoverManagerScript.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectMoverManagerScript : MonoBehaviour
 {
     [HideInInspector]
     public GameObject TargetObject;
-    public float MaxDistance = 30;
+    public float MaxDistance = 2;
     public Camera ARCamera;
     bool IsInMoveState = false;
 
@@ -18,13 +19,17 @@ public class ObjectMoverManagerScript : MonoBehaviour
     Touch Touch;
     Vector2 TouchPosition;
     quaternion RotationY;
-    float RotateSpeedModifier = 0.1f;
+    float RotateSpeedModifier = 0.01f;
+    GameObject EmptyObject;
 
     // Start is called before the first frame update
     void Start()
     {
         TargetObject = GameObject.FindGameObjectWithTag("TargetObject");
         NotObject =~ TargetObject.layer;
+        EmptyObject = new GameObject("MovePoint");
+        EmptyObject.transform.position = gameObject.transform.position + Vector3.forward * MaxDistance;
+        EmptyObject.transform.parent = ARCamera.transform;
     }
 
     // Update is called once per frame
@@ -33,26 +38,30 @@ public class ObjectMoverManagerScript : MonoBehaviour
         if (IsInMoveState == true)
         {
             //MoveObjectLogic
-            //Ray ray = ARCamera.ScreenPointToRay(new Vector3(-1/2 ,-1/2 , 0));
-            if (Physics.Raycast(ARCamera.ScreenPointToRay(new Vector3(-1/2,-1/2,0))))
-            {
-                TargetObject.transform.position = hit.point;
-                Debug.Log("Placed");
-            }
-            else if (!Physics.Raycast(ARCamera.ScreenPointToRay(new Vector3(-1 / 2, -1 / 2, 0))))
-            {
-                TargetObject.transform.position = (Camera.main.transform.eulerAngles.normalized * MaxDistance) + Camera.main.transform.position;
-                Debug.Log("Float Place");
-            }
+            //Ray ray = ARCamera.ScreenPointToRay(new Vector3(-1 / 2, -1 / 2, 0));
+            //if (Physics.Raycast(ARCamera.ScreenPointToRay(new Vector3(-1 / 2, -1 / 2, 0))))
+            //{
+            //    TargetObject.transform.position = hit.point;
+            //    Debug.Log("Placed");
+            //}
+            //else if (!Physics.Raycast(ARCamera.ScreenPointToRay(new Vector3(-1 / 2, -1 / 2, 0))))
+            //{
+            //    TargetObject.transform.position = (ARCamera.transform.eulerAngles.normalized * MaxDistance) + ARCamera.transform.position;
+            //    Debug.Log("Float Place");
+            //}
+            //Debug.Log(TargetObject.transform.position);
+            TargetObject.transform.position = EmptyObject.transform.position;
 
             //RotateObjectLogic
             if (Input.touchCount > 0)
             {
                 Touch = Input.GetTouch(0);
+                Debug.Log("Touch");
                 if (Touch.phase == TouchPhase.Moved)
                 {
-                    RotationY = quaternion.Euler(0f, Touch.deltaPosition.x * RotateSpeedModifier, 0f);
-                    transform.rotation = RotationY * transform.rotation;
+                    RotationY = quaternion.Euler(0f, -Touch.deltaPosition.x * RotateSpeedModifier, 0f);
+                    TargetObject.transform.rotation = RotationY * TargetObject.transform.rotation;
+                    Debug.Log("Rotate " +  RotationY);
                 }
             }
         }
@@ -67,6 +76,7 @@ public class ObjectMoverManagerScript : MonoBehaviour
         }
         if (!IsInMoveState)
         {
+            TargetObject.transform.parent = null;
             Debug.Log("Disable Movement");
         }
     }
